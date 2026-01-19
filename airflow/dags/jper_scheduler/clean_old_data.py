547dd5dc744b2db3e5c2fd6247d887e6a3392eaa
Anusha Ranganathan <anusha@cottagelabs.com>
2026-01-19 09:46:58 +0530

    First version that works for deletion usecase 2

---
+++ airflow/dags/jper_scheduler/clean_old_data.py
@@ -0,0 +1,69 @@
+# Python stuff
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from octopus.core import app
+from service.models.routing_history import RoutingHistory
+# Airflow stuff
+from airflow.exceptions import AirflowException, AirflowFailException, AirflowTaskTerminated
+from airflow.decorators import dag, task, task_group
+from airflow.operators.python import get_current_context
+from airflow.utils.session import provide_session
+from airflow.configuration import conf
+# My code
+from jper_scheduler.routing_deletions import RoutingDeletion
+from jper_scheduler.notification_helpers import notifications_before, iter_notifications_before
+from jper_scheduler.utils import set_task_name, get_log_url
+
+@dag(dag_id="Clean_Old_Data", max_active_runs=1,
+     schedule=None, schedule_interval=app.config.get("AIRMAINT_OLD_CLEAN", 'None'),
+     start_date=datetime(2025, 10, 22),
+     description="Cleanup all data stored older than 60 days",
+     catchup=False,
+     tags=["teamCottageLabs", "jper_cleanup"])
+def clean_old_data():
+
+    @task(task_id="get_old_routinghistory_list", retries=3, max_active_tis_per_dag=4)
+    @provide_session
+    def get_old_routinghistory_list(session=None, **context):
+        log_url = get_log_url(context)
+        app.logger.debug("Getting list of routing history IDs")
+        max_map_length = conf.getint("core", "max_map_length")
+        a = RoutingHistory()
+        since = "1970-01-01T00:00:00Z"
+        # upto = "2025-12-01T11:53:20Z"
+        upto = (datetime.now() - relativedelta(months=1)).strftime("%Y-%m-%dT%H:%M:%SZ")
+        #
+        h = a.pull_records(since=since, upto=upto, page=1, page_size=1000)
+        routing_ids = []
+        for route in h['hits']['hits']:
+            routing_history = route['_source']
+            routing_ids.append((routing_history['id'], routing_history['publisher_id']))
+        app.logger.info(f"Going to delete {len(routing_ids)} old routing history IDs")
+        app.logger.debug(f"List of routing history IDs to delete are {routing_ids}")
+        nn = notifications_before(upto, since=since)
+        app.logger.info(f"Also got {len(nn)} old notifications from helper")
+        n2 = []
+        for note in nn:
+            n2.append(note['id'])
+        app.logger.debug(f"Notifications from helper: {n2}")
+        return routing_ids[:3]
+
+    @task(task_id="delete_one_route_fileset", map_index_template="{{ map_index_template }}",
+          retries=3, max_active_tis_per_dag=4)
+    def delete_one_route_fileset(route_pub):
+        context = get_current_context()
+        log_url = get_log_url(context)
+        ti = context['ti']  # TaskInstance
+        routing_id = route_pub[0]
+        publisher_id = route_pub[1]
+        context["map_index_template"] = set_task_name(ti.map_index, routing_id)
+        app.logger.debug(f"Deleting all files for routing history id {routing_id}")
+        a = RoutingDeletion(publisher_id, routing_id=routing_id)
+        a.airflow_log_location = log_url
+        status = a.clean_all()
+        app.logger.info(f"Routing history deletion status: {status['status']}, Message: {status['message']}")
+
+    to_delete = get_old_routinghistory_list()
+    delete_one_route_fileset.expand(route_pub=to_delete)
+
+clean_old_data()
---
+++ airflow/dags/jper_scheduler/routing_deletions.py
@@ -0,0 +1,141 @@
+import os, shutil
+from service import models
+from octopus.core import app
+from octopus.modules.store import store
+from jper_scheduler.publisher_transfer import PublisherFiles
+
+dryRun = True
+
+# This class inherits from PublisherFiles (publisher_transfer.py) and will only
+# perform deletions.
+# Hopefully this will keep the publisher_transfer.py clean.
+class RoutingDeletion(PublisherFiles):
+    def __init__(self, publisher_id=None, routing_id=None):
+        if not publisher_id or not routing_id:
+            app.logger.debug(f"Invalid routing {routing_id} or publisher {publisher_id}")
+            return -1
+        super().__init__(publisher_id, routing_id=routing_id)
+
+    def _delete_file_in_server(self, remote_file):
+        # Delete one file in the sftp server
+        try:
+            if not self._is_scp:
+                self.__init_sftp_connection__()
+            # self.scp.remove(remote_file)
+            app.logger.info(f"Successfully removed {remote_file}.")
+        except Exception as e:
+            app.logger.error(f"Failed to remove {remote_file}. Error : {str(e)}")
+            return -1
+        remote_dir = os.path.dirname(remote_file)
+        try:
+            # self.scp.rmdir(remote_dir)
+            app.logger.info(f"Successfully removed {remote_dir}.")
+        except Exception as e:
+            app.logger.info(f"Failed to remove directory {remote_dir}. Error : {str(e)}")
+            app.logger.info("Directory probably not empty.")
+        return 0
+
+    # Clean file on the sftp server
+    def clean_sftp_file(self, file_name, file_location):
+        # This will be just one call per file / routing history id. So, this can be
+        # self contained with extra calls as needed.
+        # a = PublisherFiles(route['publisher_id'], routing_id=route['id'])
+        status = self._delete_file_in_server(file_name)
+        if status == 0:
+            app.logger.info(f"Successfully cleaned {file_name} from {file_location}")
+        return status
+
+    # Clean file on jper store
+    def clean_store_file(self, file_name):
+        sf = store.StoreFactory.get()
+        store_id = file_name.split("/")[5]
+        store_files = sf.list_file_paths(store_id)
+        for s_file in store_files:
+            sf.delete(store_id, s_file)
+
+    # Clean local files and directories, except the ones in "keep" locations of RoutingHistory
+    def clean_local_file(self, file_name, file_location):
+        # If I come here, the files / directory should be removed
+        if len(file_name) < 40 and file_name.count("/") < 3: # Minor sanity check
+            app.logger.warn(f"Wrongness: File name {file_name} fails basic sanity check. Skipping.")
+            return -1
+        if os.path.isfile(file_name) or os.path.islink(file_name):
+            app.logger.debug(f'Deleting file {file_name} from {file_location}')
+            # os.remove(file_name)
+        else:
+            app.logger.debug(f'Deleting directory {file_name} from {file_location}')
+            # shutil.rmtree(file_name, ignore_errors=True)
+        return 0
+
+    # Clean all files
+    def clean_all_files(self, keep=None):
+        # Only look at the "final file location" in the routing history
+        # We only clean up the physical files. We do not touch routing history itself in OpenSearch
+        for location in self.routing_history.final_file_locations:
+            file_name = location["file_location"]
+            file_location = location["location_type"]
+            if keep and isinstance(keep, list) and len(keep)>0 and file_location in keep:
+                # retain files in the above locations. They are precious.
+                app.logger.debug(f'Retain file {file_name} from {file_location}')
+                continue
+            app.logger.debug(f"--- Looking at file {file_name} in location {file_location}")
+            if file_location == "store":
+                if dryRun:
+                    app.logger.info(f"DRY RUN: Would delete store file {file_name}")
+                else:
+                    self.clean_store_file(file_name)
+            elif 'green' in file_name or 'dg_storage' in file_name:
+                if dryRun:
+                    app.logger.info(f"DRY RUN: Would delete local file {file_name}")
+                else:
+                    self.clean_local_file(file_name, file_location)
+            elif 'xfer' in file_name:
+                if dryRun:
+                    app.logger.info(f"DRY RUN: Would delete sftp file {file_name}")
+                else:
+                    self.clean_sftp_file(file_name, file_location)
+            else:
+                app.logger.warn(f"Unknown location of file : {file_name}. Doing nothing")
+        return { 'status': "success", 'message': "Cleaned up routing history ID {self.routing_history.id}" }
+
+    # Clean all notifications
+    def delete_notifications(self, note_list):
+        for notification_id in note_list:
+            notification_obj = models.RoutedNotification.pull(notification_id)
+            if notification_obj:
+                app.logger.debug(f"Deleting routed notification {notification_id}")
+                app.logger.debug(f"DRY RUN: Routed notification object: {notification_obj}")
+                if dryRun:
+                    app.logger.info(f"DRY RUN: Would delete routed notification {notification_id}")
+                else:
+                    notification_obj.delete()
+            else:
+                app.logger.debug(f"Deleting failed notification {notification_id}")
+                notification_obj = models.FailedNotification.pull(notification_id)
+                app.logger.debug(f"DRY RUN: Failed notification object: {notification_obj}")
+                if dryRun:
+                    app.logger.info(f"DRY RUN: Would delete failed notification {notification_id}")
+                else:
+                    notification_obj.delete()
+        return { 'status': "success", 'message': "Cleaned up notifications for routing id {self.routing_history.id}" }
+
+    # Clean everything for this routing history
+    def clean_all(self, keep=None):
+
+        # Clean up all the final files except the ones in "keep" locations
+        statusF = self.clean_all_files(keep=keep)
+        app.logger.info(f"File cleanup status: {statusF['status']}, Message: {statusF['message']}")
+
+        # Clean up the notifications
+        note_list = []
+        for note in self.routing_history.notification_states:
+            note_list.append(note['notification_id'])
+        if len(note_list) > 0:
+            app.logger.debug(f"Notifications to delete: {note_list}")
+            statusN = self.delete_notifications(note_list=note_list)
+        else:
+            app.logger.debug("No notifications to delete")
+            statusN = { 'status': "success", 'message': "No notifications to delete" }
+        app.logger.info(f"Notification cleanup status: {statusN['status']}, Message: {statusN['message']}")
+
+        return { 'status': "success", 'message': f"Cleaned up routing history ID {self.routing_history.id}" }
