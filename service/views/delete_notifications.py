2fdf59a1c046e4433e7253944b340d35c3d39cb4
DFG DeepGreen Project <green@vl151.kobv.de>
2026-01-20 10:00:48 +0100

    Fixed the form and message

--- service/views/delete_notifications.py
+++ service/views/delete_notifications.py
@@ -1,5 +1,6 @@
+import os
 from flask import Blueprint, request, url_for, flash, redirect, render_template, abort
-from flask_login import current_user
+from flask_login.utils import current_user
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from service.lib.validation_helper import validate_date, is_newer
@@ -13,7 +14,8 @@ def index():
     if not current_user.is_super:
         abort(401)
 
-    default_upto = (datetime.now() - relativedelta(months=6)).strftime("%d/%m/%Y")
+    default_upto = validate_date((datetime.now() - relativedelta(months=6)).strftime("%d/%m/%Y"),
+                                 param='upto')
 
     if request.method == 'GET':
         upto = validate_date(default_upto, param='upto')
@@ -29,7 +31,7 @@ def index():
     # status values
     accepted_status_values = ['success-routed', 'success-failed', 'failure']
     status_values = []
-    for s in request.values.getlist('status', []):
+    for s in request.form.getlist('status'):
         if s.lower() in accepted_status_values:
             status_values.append(s.lower())
 
@@ -38,12 +40,14 @@ def index():
     if upto == '' or upto is None:
         upto = default_upto
     upto = validate_date(upto, param='upto')
+
     if is_newer(upto, default_upto):
         flash(f"date {upto} has to be older than 6 months")
         return render_template('delete_notifications/index.html', publisher_id=publisher_id,
                            upto=default_upto, status_values=status_values)
 
     #ToDo: Call airflow dag here to delete with these params
+    os.system("touch /tmp/aaaa.aaa")
 
     return render_template('delete_notifications/deletion_sent.html', publisher_id=publisher_id,
                            upto=upto, status_values=status_values)
