a833eb5e95c0468a705a04f789a8b3fdf81a3ca9
Anusha Ranganathan <anusha@cottagelabs.com>
2026-04-22 19:32:20 +0530

    Do the notification repackaging and limit maxnumber of notifications to process

--- airflow/dags/jper_scheduler/regenerate_metsmods.py
+++ airflow/dags/jper_scheduler/regenerate_metsmods.py
@@ -1,28 +1,19 @@
-# Python stuff
+# Python imports
 from datetime import datetime
-import math
-import re
-from dateutil.relativedelta import relativedelta
+# jper stuff
 from octopus.core import app
-from service.models.routing_history import RoutingHistory
+from service.lib.repackage_notifications import repackage_notification
+from jper_scheduler.utils import set_task_name, get_log_url
 # Airflow stuff
 from airflow.exceptions import AirflowException, AirflowFailException, AirflowTaskTerminated
-from airflow.decorators import dag, task, task_group
+from airflow.decorators import dag, task
 from airflow.operators.python import get_current_context
-from airflow.utils.session import provide_session
 from airflow.configuration import conf
-# My code
-from jper_scheduler.routing_deletions import RoutingDeletion
-from jper_scheduler.notification_helpers import notifications_before, iter_notifications_before
-from jper_scheduler.utils import set_task_name, get_log_url
-from service import models
 
 @dag(dag_id="Regenerate_MetsMods", max_active_runs=1,
      schedule=None, schedule_interval=app.config.get("AIRFLOW_REGENERATE_METSMODS_SCHED", 'None'),
-     start_date=datetime(2025, 10, 22),
-     description="Regenerate METS/MODS for notifications on demand",
-     catchup=False,
-     tags=["teamCottageLabs", "regenerate_ondemand"])
+     start_date=datetime(2025, 10, 22), description="Regenerate METS/MODS on demand for given notifications",
+     catchup=False, tags=["teamCottageLabs", "regenerate_ondemand"])
 def regenerate_metsmods():
     # Regenerate METS/MODS for notifications on demand - called using REST api or Airflow UI
     @task(task_id="list_of_notifications", retries=0, max_active_tis_per_dag=1)
@@ -39,6 +30,12 @@ def regenerate_metsmods():
             app.logger.info("Missing parameters for on-demand METS/MODS regeneration - exiting")
             return []
 
+        max_length = conf.getint("core", "max_map_length")        
+        if len(notifications_list) > max_length:
+            app.logger.info(f"Too many notifications given for on-demand METS/MODS regeneration - exiting.")
+            app.logger.info(f"Max allowed is {max_length}, but received {len(notifications_list)}")
+            raise AirflowFailException(f"Too many notifications given for on-demand METS/MODS regeneration - exiting.")
+
         app.logger.info(f"Parameters to search for routing history records:")
         app.logger.info(f"notifications_list: {notifications_list}")
         app.logger.info(f"format: {format}")
@@ -47,13 +44,11 @@ def regenerate_metsmods():
     @task(task_id="regenerate_metsmods_for_notification", retries=0, max_active_tis_per_dag=1)
     def regenerate_metsmods_for_notification(notification_id):
         context = get_current_context()
-        log_url = get_log_url(context)
         format = context['params'].get("format")
-        print(f"Starting METS/MODS regeneration for notification {notification_id}")
-        print(f"Format requested: {format}")
-        print(f"Logs available at {log_url}")
-        ti = context['ti']  # TaskInstance
+        app.logger.info(f"Doing METS/MODS regeneration for ALL repos notification {notification_id} with format {format}")
+        ti = context['ti']
         context["map_index_template"] = set_task_name(ti.map_index, f"{format} {notification_id}")
+        repackage_notification(notification_id, repo_ids=[], packaging_formats=[format], add_new_links=True)
         return
 
     notification_list = list_of_notifications_ondemand()
