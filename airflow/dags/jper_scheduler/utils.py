8a03dcb239acae8eb6586abb98fec0f6950f8076
Anusha Ranganathan <anusha@cottagelabs.com>
2026-07-03 07:43:50 +0530

    Get printout in single line instead of 3

--- airflow/dags/jper_scheduler/utils.py
+++ airflow/dags/jper_scheduler/utils.py
@@ -261,9 +261,7 @@ def utils_log_routing_history(rh):
 
 
 def create_routing_history_record(note_index, notification_id, log_url=None):
-    app.logger.info(
-        f"Creating routing history record for notification ID {notification_id}"
-    )
+    app.logger.info(f"Creating routing history record for notification ID {notification_id}")
 
     obj = None
     matches = 0
