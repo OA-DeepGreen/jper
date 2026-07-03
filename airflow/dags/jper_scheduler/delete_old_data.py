c6aaecc335b87aaca3bb55241a5d072c997c74c7
Anusha Ranganathan <anusha@cottagelabs.com>
2026-07-03 18:17:16 +0530

    Add routing history if needed

--- airflow/dags/jper_scheduler/delete_old_data.py
+++ airflow/dags/jper_scheduler/delete_old_data.py
@@ -1,11 +1,9 @@
 # Python stuff
-import os
+from logging import info
 import math
-import json
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from octopus.core import app
-from service import models
 import esprit
 from service.models.routing_history import RoutingHistory
 # Airflow stuff
@@ -26,7 +24,7 @@ page_size = 2000
 port = host.split(':')[-1]
 host_name = host.split(port)[0][:-1]
 
-def get_all_notifications(index, since, upto, publisher_id, log_url=None):
+def get_all_notifications(index, since, upto, publisher_id):
     # Also create a routing history record for each notification if needed
     conn = esprit.raw.Connection(host_name, index, port=port)
     records = get_notifications_for(
@@ -43,17 +41,11 @@ def get_all_notifications(index, since, upto, publisher_id, log_url=None):
         app.logger.info("No records returned from open search matching query - exiting")
         return []
     info_to_run = []
-    b = RoutingHistory()
 
     for hit in records["hits"]["hits"]:
         notification_id = hit["_id"]
         index = hit["_index"]
-        info_to_run.append((notification_id, index))
-        c = b.pull_record_for_notification(notification_id)
-        if not c: # No routing history found - create a new record
-            app.logger.info(f"No routing history for notification {notification_id} - creating a routing history record now")
-            create_routing_history_record(index, note_id, log_url=log_url)
-
+        info_to_run.append([notification_id, index])
 
     if num_records > page_size:
         page = 2
@@ -76,13 +68,19 @@ def get_all_notifications(index, since, upto, publisher_id, log_url=None):
             for hit in records["hits"]["hits"]:
                 notification_id = hit["_id"]
                 index = hit["_index"]
-                info_to_run.append((notification_id, index))
-                c = b.pull_record_for_notification(notification_id)
-                if not c: # No routing history found - create a new record
-                    app.logger.info(f"No routing history for notification {notification_id} - creating a routing history record now")
-                    create_routing_history_record(index, note_id, log_url=log_url)
+                info_to_run.append([notification_id, index])
     return info_to_run
 
+def check_add_routing_history(b, publisher, note_id, index, status, log_url):
+    routing_id = ""
+    c = b.pull_record_for_notification(note_id)
+    if not c: # No routing history found - create a new record
+        app.logger.info(f"No routing history for notification {note_id} - creating a routing history record now")
+        rh_info = create_routing_history_record(index, note_id, log_url=log_url)
+        routing_id = rh_info[0]
+    else:
+        routing_id = c.id
+    return(publisher, note_id, routing_id, status)
 
 @dag(dag_id="Delete_Old_Data",
     max_active_runs=1,
@@ -128,6 +126,7 @@ def clean_old_data():
         notes_routed = []
         notes_failed = []
         notes_errored = []
+        info_to_process = []
         since = "1970-01-01T00:00:00Z"
 
         if b.retain_routed <= 0:
@@ -135,14 +134,14 @@ def clean_old_data():
         else:
             upto = (datetime.now() - relativedelta(months=b.retain_routed)).strftime('%Y-%m-%dT%H:%M:%SZ')
             index = "jper-routed*"
-            notes_routed = get_all_notifications(index, since, upto, publisher_id=publisher, log_url=log_url)
+            notes_routed = get_all_notifications(index, since, upto, publisher_id=publisher)
 
         if b.retain_failed <= 0:
             app.logger.info(f"Retaining all failed notifications for publisher {publisher}")
         else:
             upto = (datetime.now() - relativedelta(months=b.retain_failed)).strftime('%Y-%m-%dT%H:%M:%SZ')
             index = "jper-failed*"
-            notes_failed = get_all_notifications(index, since, upto, publisher_id=publisher, log_url=log_url)
+            notes_failed = get_all_notifications(index, since, upto, publisher_id=publisher)
 
         if b.retain_errored <= 0:
             app.logger.info(f"Retaining all errored notifications for publisher {publisher}")
@@ -159,10 +158,11 @@ def clean_old_data():
                     app.logger.info("No records returned from open search matching query")
                 else:
                     for hit in records.get('hits', {}).get('hits', []):
+                        routing_id = hit["id"]
                         for note_state in hit.get("notification_states", []):
                             notification_id = note_state.get("notification_id", "None")
                             if notification_id:
-                                notes_errored.append(notification_id)
+                                notes_errored.append((publisher, notification_id, routing_id, "errored"))
 
         if len(notes_routed) > 3:
             notes_routed = notes_routed[:3]
@@ -170,8 +170,21 @@ def clean_old_data():
             notes_failed = notes_failed[:3]
         if len(notes_errored) > 3:
             notes_errored = notes_errored[:3]
-        print(f"notes_routed: {notes_routed}, notes_failed: {notes_failed}, notes_errored: {notes_errored}")
-        return (publisher, notes_routed, notes_failed, notes_errored)
+
+        b = RoutingHistory()
+        for note_info in notes_routed:
+            note_id = note_info[0]
+            index = note_info[1]
+            info_to_process.append(check_add_routing_history(b, publisher, note_id, index, "success", log_url))
+        for note_info in notes_failed:
+            note_id = note_info[0]
+            index = note_info[1]
+            info_to_process.append(check_add_routing_history(b, publisher, note_id, index, "failed", log_url))
+        for note_info in notes_errored: # Keep this here for code clarity
+            info_to_process.append(note_info)
+
+        print(f"info_to_process: {info_to_process}")
+        return info_to_process
 
     @task(task_id="notes_list_all_publishers", retries=3, max_active_tis_per_dag=4, trigger_rule="all_done")
     def notes_list_all_publishers(files, session=None):
@@ -179,18 +192,12 @@ def clean_old_data():
         # Note the trigger rule, to cover the eventuality that one or more publishers could have bad initialisation
         # We are working with xcom objects. Hence need for a separate function to concatenate the lists
         context = get_current_context()
-        log_url = get_log_url(context)
         max_map_length = conf.getint("core", "max_map_length")
         notes_to_delete = []
         if files: # Protect if there are no files to transfer
             for file in files:
-                publisher_id = file[0]
-                for routed in file[1]:
-                    notes_to_delete.append((publisher_id, routed, "success"))
-                for failed in file[2]:
-                    notes_to_delete.append((publisher_id, failed, "failed"))
-                for errored in file[3]:
-                    notes_to_delete.append((publisher_id, errored, "errored"))
+                if file:
+                    notes_to_delete.extend(file)
         else:
             app.logger.warn("Empty run")
             dag_run = session.merge(context['dag_run'])
@@ -205,13 +212,17 @@ def clean_old_data():
 
     @task(task_id="delete_one_notification", retries=0, max_active_tis_per_dag=4)
     def delete_one_notification(note_tuple):
-        publisher_id, note_tuple, status = note_tuple
-        note_id = note_tuple[0]
-        index = note_tuple[1]
+        context = get_current_context()
+        log_url = get_log_url(context)
+        print(note_tuple)
+        publisher_id = note_tuple[0]
+        note_id = note_tuple[1]
+        routing_id = note_tuple[2]
+        status = note_tuple[3]
         app.logger.info(f"Deleting notification {note_id} for publisher {publisher_id} with status ({status})")
         context = get_current_context()
         log_url = get_log_url(context)
-        a = RoutingDeletion(publisher_id=publisher_id, note_id=note_id, index=index, log_url=log_url)
+        a = RoutingDeletion(publisher_id=publisher_id, routing_id=routing_id)
         a.airflow_log_location = log_url
         deletion_reason = "Old data cleanup as requested in publisher configuration page"
         a.clean_all(notification_id=note_id, status_values=[status], deletion_reason=deletion_reason)
