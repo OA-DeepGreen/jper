21c60f7f974bc28defab6e4b05d76a7175c081a1
Anusha Ranganathan <anusha@cottagelabs.com>
2026-09-06 15:25:07 +0530

    Remove local routing history save to allow using bulk methods for opensearch indexing

--- airflow/dags/jper_scheduler/utils.py
+++ airflow/dags/jper_scheduler/utils.py
@@ -373,9 +373,9 @@ def create_routing_history_record(note_index, notification_id, log_url=None):
     return (rh.id, rh.publisher_id, doi)
 
 def create_routing_history_record_for_del(note_info, log_url=None):
-    app.logger.info(f"Creating routing history record for deletion of notification ID {notification_id}")
+    # app.logger.info(f"Creating routing history record for deletion of notification ID {note_info['id']}")
 
-    note_info_needed = "note.repositories", doi,
+    # note_info_needed = "note.repositories", doi,
 
     notification_id = note_info["id"]
 
@@ -386,9 +386,9 @@ def create_routing_history_record_for_del(note_info, log_url=None):
         note_state = "failure"
 
     doi = note_info["doi"]
-    app.logger.info(
-        f"Notification ID {notification_id} has DOI {doi} and matches {matches} repositories"
-    )
+    # app.logger.info(
+    #     f"Notification ID {notification_id} has DOI {doi} and matches {matches} repositories"
+    # )
 
     rh = RoutingHistory()
     rh.id = uuid.uuid4().hex
@@ -398,10 +398,10 @@ def create_routing_history_record_for_del(note_info, log_url=None):
     rh.sftp_server_url = note_info["sftp_url"]
     rh.sftp_server_port = note_info["sftp_port"]
     rh.sftp_username = note_info["sftp_username"]
-    app.logger.debug(f"Publisher : {rh.publisher_id}, {rh.publisher_email}")
-    app.logger.debug(
-        f"SFTP info : URL {rh.sftp_server_url}, Port {rh.sftp_server_port}, Username {rh.sftp_username}"
-    )
+    # app.logger.debug(f"Publisher : {rh.publisher_id}, {rh.publisher_email}")
+    # app.logger.debug(
+    #     f"SFTP info : URL {rh.sftp_server_url}, Port {rh.sftp_server_port}, Username {rh.sftp_username}"
+    # )
     rh.original_file_location = "None"
     rh.final_file_locations = []
     rh.notification_states = [
@@ -422,9 +422,9 @@ def create_routing_history_record_for_del(note_info, log_url=None):
     )
 
     if store.StoreFactory.get().exists(notification_id):
-        app.logger.info(
-            f"Found record in store. Adding file locations to routing history for notification id: {notification_id}"
-        )
+        # app.logger.info(
+        #     f"Found record in store. Adding file locations to routing history for notification id: {notification_id}"
+        # )
         store_files = store.StoreFactory.get().list_file_paths(notification_id)
         for index, s_file in enumerate(store_files):
             rh.add_final_file_location("store", s_file)
@@ -433,22 +433,22 @@ def create_routing_history_record_for_del(note_info, log_url=None):
                 file_location=s_file,
                 notification_id=notification_id,
                 status="success",
-                message="Reprocessed old notification, added file locations from store",
+                message="Reprocessed old notification for deletion, added file locations from store",
                 log_url=log_url,
             )
     else:
-        app.logger.info(
-            f"No record found in store for notification id: {notification_id}. Setting file location to None."
-        )
+        # app.logger.info(
+        #     f"No record found in store for notification id: {notification_id}. Setting file location to None."
+        # )
         rh.add_workflow_state(
             action=f"No Store file",
             file_location="None",
             notification_id=notification_id,
             status="success",
-            message="Reprocessed old notification, no file location found in store",
+            message="Reprocessed old notification for deletion, no file location found in store",
             log_url=log_url,
         )
 
-    rh.save()
+    # rh.save()
     # return rh
-    return (rh.id, rh.publisher_id, doi)
+    return rh
